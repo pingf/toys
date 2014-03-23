5f101897a8d348ecb0c7fb2fb2db2f6db817bb2a
jesse <pingf0@gmail.com>
2014-03-24 01:36:51 +0800

    fix dec on class

--- src/201403/dec_class.py
+++ src/201403/dec_class.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 __author__ = 'jesse'
 
 
@@ -11,7 +12,7 @@ class Demo():
             print 'after'
         @classmethod
         def dec_method(cls,words):
-            def true_method(self):
+            def true_method(method):
                 return real_method
             def real_method(self,name):
                 cls.before()
@@ -19,6 +20,19 @@ class Demo():
                 cls.after()
                 return ret
             return true_method
+
+        @classmethod
+        def dec_func2(cls,method):
+            def real_dec_func2(self,name):
+
+
+                cls.before()
+                method(self,name)
+
+                cls.after()
+
+            return real_dec_func2
+
 
     def __init__(self):
         pass
@@ -29,7 +43,13 @@ class Demo():
     @DecClass.dec_method('real_test')
     def test(self,name): pass
 
+
+    @DecClass.dec_func2
+    def test2(self,name):
+        print name
+
 if __name__ == "__main__":
     t=Demo()
-    t.test('123')
+    t.test('123')#test方法是我眩晕状态下写出来的，不用看了
     t.test('456')
+    t.test2('789')
--- src/201403/dec_class02.py
+++ src/201403/dec_class02.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 __author__ = 'jesse'
 
 def before():
@@ -6,15 +7,26 @@ def after():
     print 'after'
 
 def dec_func(method):
-    def true_method(self):
+    def true_method(method):
         return real_method
     def real_method(self,param):
         before()
         ret = getattr(self,method)(param)
         after()
         return ret
-
     return true_method
+
+def dec_func2(method):
+    def real_dec_func2(self,name):
+
+        print 'before'
+        method(self,name)
+        print 'after'
+
+    return real_dec_func2
+
+
+
 class Demo():
     def __init__(self):
         pass
@@ -24,7 +36,11 @@ class Demo():
     @dec_func('real_test')
     def test(self,name):pass
 
+    @dec_func2
+    def test2(self,name):
+        print name
 if __name__ == "__main__":
     t=Demo()
-    t.test('123')
+    t.test('123')#test方法是我眩晕状态下写出来的，不用看了
     t.test('456')
+    t.test2('789')
