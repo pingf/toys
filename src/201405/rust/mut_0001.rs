8f13480357b9c75de016309039b6d64b2926f7b3
jesse <pingf0@gmail.com>
2014-05-14 00:20:40 +0800

    add simple rust demos for using of mut

---
+++ src/201405/rust/mut_0001.rs
@@ -0,0 +1,12 @@
+/******
+ * This file is generated with RustyCage
+ */
+#[main]
+fn run() {
+  spawn(hello);
+}
+
+fn hello() {
+  let x=1;
+  //x=2;
+}
---
+++ src/201405/rust/mut_0002.rs
@@ -0,0 +1,13 @@
+/******
+ * This file is generated with RustyCage
+ */
+#[main]
+fn run() {
+  spawn(hello);
+}
+
+fn hello() {
+    let mut x;
+    x=2; 
+    println!("{}",x);
+}
